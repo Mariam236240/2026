a0acb52038eb054ef2217af5966022f8004c2722
Mariam236240 <karemalotfy7uuo@gmail.com>
2025-11-27 20:58:05 +0200

    Update vite.config.js

--- clothees_vite/vite.config.js
+++ clothees_vite/vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()]
+});aimport { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/2026/"  // 🔥 ADD THIS LINE
 });
